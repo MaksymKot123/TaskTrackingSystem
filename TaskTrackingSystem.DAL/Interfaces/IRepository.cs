95c9d6990cabd06b14f3bd8696279e455dc0ebe2
Maksym Kot <rabbit0165@gmail.com>
2021-05-06 22:12:53 +0300

    added IRepository

---
+++ TaskTrackingSystem.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaskTrackingSystem.DAL.Models;
+
+namespace TaskTrackingSystem.DAL.Interfaces
+{
+    public interface IRepository<T> : IDisposable where T : class
+    {
+        T Get(int? id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Func<T, bool> func);
+        void Create(T item);
+        void Edit(T item);
+        void Delete(T item);
+    }
+}
