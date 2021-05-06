513459836b7df55b5a2ca2c2b37cb956b442e6f4
Maksym Kot <rabbit0165@gmail.com>
2021-05-06 22:40:14 +0300

    added repository for tasks

---
+++ TaskTrackingSystem.DAL/Repositories/TaskRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using TaskTrackingSystem.DAL.Models;
+using TaskTrackingSystem.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskTrackingSystem.DAL.Repositories
+{
+    public class TaskRepository : IRepository<TaskProject>
+    {
+        public DatabaseContext db { get; set; }
+
+        public TaskRepository(DatabaseContext context)
+        {
+            db = context;
+        }
+
+        public void Create(TaskProject item)
+        {
+            db.Tasks.Add(item);
+            db.SaveChanges();
+        }
+
+        public void Delete(TaskProject item)
+        {
+            db.Tasks.Remove(item);
+            db.SaveChanges();
+        }
+
+        public void Edit(TaskProject item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+
+        public TaskProject Get(int? id)
+        {
+            if (id == null)
+                return null;
+            else
+                return db.Tasks.Find(id);
+        }
+
+        public IEnumerable<TaskProject> GetAll() => db.Tasks;
+
+        public IEnumerable<TaskProject> Find(Func<TaskProject, bool> func)
+        {
+            return db.Tasks.Where(func);
+        }
+    }
+}
