17bde4f8994fab7055054f5e73eb2b0b3dcd384b
Maksym Kot <rabbit0165@gmail.com>
2021-05-07 11:08:42 +0300

    added interface for project service

---
+++ TaskTrackingSystem.BLL/Interfaces/IProjectService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskTrackingSystem.BLL.DTO;
+
+namespace TaskTrackingSystem.BLL.Interfaces
+{
+    public interface IProjectService : IDisposable
+    {
+        IEnumerable<ProjectDTO> GetAllProjects();
+        IEnumerable<ProjectDTO> Find(Func<ProjectDTO, bool> func);
+        ProjectDTO GetProject(string name);
+        void EditProject(ProjectDTO project);
+    }
+}
