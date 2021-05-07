346c785e6e0ecca8dadca1db508d95e880f934e9
Maksym Kot <rabbit0165@gmail.com>
2021-05-07 11:05:00 +0300

    added enum for dto

---
+++ TaskTrackingSystem.BLL/Enums/StatusDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskTrackingSystem.BLL.Enums
+{
+    public enum StatusDTO
+    {
+        Pending,
+        Started,
+        OnProgress,
+        Completed
+    }
+}
