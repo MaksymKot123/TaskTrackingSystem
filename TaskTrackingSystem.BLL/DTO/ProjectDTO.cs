f5057c6f4abeefea83261aac045d36263fd3985b
Maksym Kot <rabbit0165@gmail.com>
2021-05-07 11:03:31 +0300

    added DTO models for task and project

---
+++ TaskTrackingSystem.BLL/DTO/ProjectDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskTrackingSystem;
+
+namespace TaskTrackingSystem.BLL.DTO
+{
+    public class ProjectDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public DAL.Enums.Status Status { get; set; }
+        public string Description { get; set; }
+        public string ClientEmail { get; set; }
+        public double PercentCompletion { get; set; }
+        public virtual ICollection<UserDTO> Employees { get; set; }
+        public virtual ICollection<TaskDTO> Tasks { get; set; }
+    }
+}
---
+++ TaskTrackingSystem.BLL/DTO/TaskDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TaskTrackingSystem;
+
+namespace TaskTrackingSystem.BLL.DTO
+{
+    public class TaskDTO
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public DAL.Enums.Status Status { get; set; }
+        public string Description { get; set; }
+        public virtual ProjectDTO Project { get; set; }
+    }
+}
