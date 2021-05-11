b9ccff0eff51724987bef6b9475f0a9845ab0532
Maksym Kot <rabbit0165@gmail.com>
2021-05-11 15:27:11 +0300

    method for registration has been added

--- TaskTrackingSystem.BLL/Registration/Registration.cs
+++ TaskTrackingSystem.BLL/Registration/Registration.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskTrackingSystem.BLL.DTO;
 using TaskTrackingSystem.DAL.Interfaces;
 using TaskTrackingSystem.DAL.Models;
+using AutoMapper;
 
 namespace TaskTrackingSystem.BLL.Registation
 {
@@ -10,11 +13,50 @@ namespace TaskTrackingSystem.BLL.Registation
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly DatabaseContext _database;
+        private readonly IMapper _mapper;
 
-        public Registration(IUnitOfWork unitOfWork, DatabaseContext database)
+        public Registration(IUnitOfWork unitOfWork, DatabaseContext database, IMapper mapper)
         {
             _unitOfWork = unitOfWork;
             _database = database;
+            _mapper = mapper;
+        }
+
+        public async Task<UserDTO> Register(UserDTO newUser, string password)
+        {
+            if (_unitOfWork.UserManager.Users.Any(x => x.Email.Equals(newUser.Email)))
+            {
+
+            }
+            else
+            {
+                var usr = new User()
+                {
+                    Email = newUser.Email,
+                    Name = newUser.Name,
+                    UserName = newUser.Email,
+                };
+
+                var res = await _unitOfWork.UserManager.CreateAsync(usr, password);
+
+                if (res.Succeeded) 
+                {
+                    var userFromDatabase = await _unitOfWork.UserManager
+                        .FindByEmailAsync(usr.Email);
+
+                    return new UserDTO()
+                    {
+                        Email = userFromDatabase.Email,
+                        Id = userFromDatabase.Id,
+                        Name = userFromDatabase.Name,
+                        Projects = _mapper.Map<ICollection<ProjectDTO>>(
+                            userFromDatabase.Projects),
+                        
+                    };
+                }
+            }
+
+            
         }
     }
 }
