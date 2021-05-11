1dedd7a61fc4079bf61e3965e4dcab90d3542d5a
Maksym Kot <rabbit0165@gmail.com>
2021-05-11 15:14:54 +0300

    added class for regisration

---
+++ TaskTrackingSystem.BLL/Registration/Registration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskTrackingSystem.DAL.Interfaces;
+using TaskTrackingSystem.DAL.Models;
+
+namespace TaskTrackingSystem.BLL.Registation
+{
+    public class Registration
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly DatabaseContext _database;
+
+        public Registration(IUnitOfWork unitOfWork, DatabaseContext database)
+        {
+            _unitOfWork = unitOfWork;
+            _database = database;
+        }
+    }
+}
