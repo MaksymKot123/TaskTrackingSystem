19cb7f7f996da303371a25fea3f078546dccfb3f
Maksym Kot <rabbit0165@gmail.com>
2021-05-14 18:24:52 +0300

    settings for routes were added to app.module.ts

--- TaskTrackingSystem/ClientApp/src/app/app.module.ts
+++ TaskTrackingSystem/ClientApp/src/app/app.module.ts
@@ -11,10 +11,20 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { Routes, RouterModule } from "@angular/router";
+import { AdminComponent } from './AdminComponent/admin.component';
+
+const adminRoutes: Routes = [
+  { path: '', component: ProjectsComponent },
+  { path: 'addproject', component: AddProjectComponent },
+  { path: 'deleteproject', component: DeleteProjectComponent }
+]
 
 const appRoutes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'reg', component: RegisterComponent },
+  { path: 'admin', component: AdminComponent, children: adminRoutes },
+  { path: 'manager', component: ManagerComponent, children: managerRoutes },
+  { path: 'employee', component: EmployeeComponent, children: employeeRoutes },
 ];
 
 
