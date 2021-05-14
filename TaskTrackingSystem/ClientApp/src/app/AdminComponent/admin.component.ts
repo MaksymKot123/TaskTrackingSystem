85f2507b2e344838b4fca81b0d64dee3c22cbb5a
Maksym Kot <rabbit0165@gmail.com>
2021-05-14 23:46:38 +0300

    3

--- TaskTrackingSystem/ClientApp/src/app/AdminComponent/admin.component.ts
+++ TaskTrackingSystem/ClientApp/src/app/AdminComponent/admin.component.ts
@@ -1,9 +1,14 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'admin',
   templateUrl: './admin.component.html',
 })
 export class AdminComponent {
-
+  constructor(private router: Router) {
+    if (localStorage.getItem("role") !== "Admin") {
+      this.router.navigateByUrl("");
+    }
+  }
 }
