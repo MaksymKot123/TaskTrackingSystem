2eda246c270002ecf70eae41597bdfe5f4c8a117
Maksym Kot <rabbit0165@gmail.com>
2021-05-20 01:04:35 +0300

    class was added for sending emails to clients

---
+++ TaskTrackingSystem.BLL/EmailSender/EmailSender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+
+namespace TaskTrackingSystem.BLL.EmailSender
+{
+    public static class EmailSender
+    {
+        const string MailAddress = "tasktrackingsystememailsender@gmail.com";
+        const string MailPassword = "qwe123Aa";
+        const string SmtpAddress = "smtp.gmail.com";
+        const int SmtpPort = 587;
+        const string EmailSenderName = "Task system email sender";
+
+        public static void SendEmail(string clientEmail)
+        {
+            var from = new MailAddress(MailAddress, EmailSenderName);
+            var to = new MailAddress(clientEmail);
+
+            var mail = new MailMessage(from, to);
+
+
+            var smtp = new SmtpClient(SmtpAddress, SmtpPort);
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = new NetworkCredential(MailAddress, MailPassword);
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
+        }
+    }
+}
